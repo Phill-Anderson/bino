62497b4fbd30daf924cff6decbf7c5f74753ea7b
Phill Anderson <eaglesquad86@gmail.com>
2021-03-06 19:35:10 +0800

    Mobile menu js кодыг бичив

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,19 +1,21 @@
 class MobileMenu {
   constructor() {
-    this.menuIcon = document.querySelector(".site-header__menu-icon");
-    this.menuContent = document.querySelector(".site-header__menu-content");
-    this.siteHeader = document.querySelector(".site-header");
+    this.menuIcon = document.querySelector(".mobile-menu");
+    this.menuContent = document.querySelector(".mobile-menu__content");
+    this.siteHeader = document.querySelector(".header");
     this.events();
   }
 
   events() {
-    this.menuIcon.addEventListener("click", () => this.toggleTheMenu());
+    this.menuIcon.addEventListener("click", () => this.toggleMenu());
   }
 
-  toggleTheMenu() {
-    this.menuContent.classList.toggle("site-header__menu-content--is-visible");
-    this.siteHeader.classList.toggle("site-header--is-expanded");
-    this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
+  toggleMenu() {
+    this.menuContent.classList.toggle("mobile-menu__content--is-visible");
+    if (this.menuContent.style.display === 'block') this.menuContent.style.display = "none"
+    else this.menuContent.style.display = "block";
+    this.siteHeader.classList.toggle("mobile-menu--is-expanded");
+    this.menuIcon.classList.toggle("mobile-menu--closed");
   }
 }
 
